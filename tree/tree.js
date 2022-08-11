d4aa753ac32303de3f9395425ec149bec7733aa1
matdombrock <dombrock.mathieu@gmail.com>
2022-08-11 02:49:48 +0000

    mathie edits

--- tree/tree.js
+++ tree/tree.js
@@ -43,74 +43,85 @@ module.exports = {
         r:'init_email'
     },
     init_email:{
-        text: `Hello, I'm \`Ralph\` the Canvasser app bot! I'm going to try to help you out. 
+        text: `Hello, thanks for sending me your issue. I'm \`Ralph\` the Canvasser app bot, with Manis Pierre at Voter-Science. We're going to help fix your app. 
         ${nl}First, I need to ask a few questions and have you send me the answers. 
         ${nl}--------------
         ${nl}Please tell me your canvasser app login email address:`,
+        l:'init_phone_make_model',
+        r:'init_phone_make_model'
+    },
+    init_phone_make_model:{
+        text: `Please text me the make and model of your phone:`,
+        l:'init_time',
+        r:'init_time'
+    },
+    init_time:{
+        text: `Please text me the approximate time the issue occurred:`,
         l:'init_location',
         r:'init_location'
+        
     },
     init_location:{
-        text: `Please tell me your location:`,
+        text: `Please tell me the location the issue first happened:`,
         l:'init_walkbook',
         r:'init_walkbook'
     },
     init_walkbook:{
-        text: `Please tell me your walkbook name:`,
+        text: `Please tell me the walkbook you were in or the walkbook you were attempting to access when the issue occurred:`,
         l:'init_precinct',
         r:'init_precinct'
     },
     init_precinct:{
-        text: `Please tell me your precinct name:`,
-        l:'init_phone_model',
-        r:'init_phone_model'
+        text: `Please tell me the precinct you were in or the precinct you were attempting to access when the issue occurred:`,
+        l:'init_contact',
+        r:'init_contact'
     },
-    init_phone_model:{
-        text: `Please tell me your phone model:`,
+    init_contact:{
+        text: `Please text me the best way to reach you in case Manis needs to call or email you:`,
         l:'start',
         r:'start'
     },
     start:{
-        text: `From now on, I'll ask you questions and you can reply with 'yes', 'y', 'no' or 'n'.
+        text: `From here, I'll ask you questions, please reply with 'yes', 'y', 'no' or 'n'.
         ${nl}-------------- 
-        ${nl}Does your Canvasser App Open?`,
+        ${nl}Does your Canvasser App remain Open?`,
         l:'data_save',
         r: 'end_uninstall_reinstall'
     },
     data_save:{
-        text: `Does your data save?`,
+        text: `Does your data save while you canvass?`,
         l:'gps_issue',
         r:'free_space'
     },
     gps_issue:{
-        text: `Is there a GPS issue?`,
+        text: `Is there a GPS issue or GPS dot issue?`,
         l:'pin_fade',
         r:'happy_profit',
     },
     pin_fade:{
-        text: `Do your pins not fade?`,
+        text: `Do your house pins not fade?`,
         l:'end_screenshot',
         r:'wrong_house',
     },
     wrong_house:{
-        text: `Does it load the wrong house under specific settings?`,
+        text: `Does it load the wrong house pins but right address?`,
         l:'end_clear_searches',
         r:'end_screenshot',
     },
     happy_profit:{
-        text: `Does it load the wrong house under specific settings?`,
+        text: `Does it load the right house pins but wrong address?`,
         l:'end_cash_in',
         r:'end_explain_happy_profit',
     },
     free_space:{
-        text: `Does yout phone have 80% or more free space?`,
+        text: `Does your phone have 80% or more free space?`,
         l:'end_uninstall_reinstall',
         r:'end_contact_rsd',
     },
     
     // End Nodes
     end_screenshot:{
-        text: `Please take a screenshot of the issue and email it to Manis@wsnrg.com.`,
+        text: `Please take a screenshot of the issue and email it to Manis@Voter-Science.com.`,
         l:'end_resolution_n',
         r:'end_resolution_n',
     },
@@ -127,28 +138,28 @@ module.exports = {
         r:'end_resolution_n',
     },
     end_contact_rsd:{
-        text: `Please contact your RFD and Manis Pierre. 
-        ${nl}Does this resolve your issue?`,
+        text: `Manis will reach out to you as soon as possible. 
+        ${nl}We're sorry for any inconvenience`,
         l:'end_resolution_y',
         r:'end_resolution_n',
     },
     end_cash_in:{
-        text: `You are cashing in. (idk what this should say) 
-        ${nl}Does this resolve your issue?`,
+        text: `Thank you for using the Canvasser text chat bot.  
+        ${nl}We're sorry for any inconvenience`,
         l:'end_resolution_y',
         r:'end_resolution_n',
     },
     end_explain_happy_profit:{
-        text: `This is an explanation about happy profit. (idk what this should say) 
-        ${nl}Does this resolve your issue?`,
+        text: `Thank you for using the Canvasser text chat bot.  
+        ${nl}We're sorry for any inconvenience`,
         l:'end_resolution_y',
         r:'end_resolution_n',
     },
 
     // Resolution nodes
     end_resolution_n:{
-        text: `Sorry, that's all I know how to do right now... 
-        ${nl}I guess I will just restart now.`,
+        text: `Manis will reach out to you as soon as possible. 
+        ${nl}We apologize for any inconvenience.`,
         l:'init',
         r:'init',
     },
