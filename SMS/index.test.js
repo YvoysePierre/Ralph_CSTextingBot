afed5cf8b91722911b13eea38f278afa7422cd6a
Manis Pierre <YvoysePierre@users.noreply.github.com>
2022-12-08 20:42:52 -0800

    Update index.test.js

--- SMS/index.test.js
+++ SMS/index.test.js
@@ -2,7 +2,7 @@ const SMS = require('./index');
 
 async function test()
 {
-    const res = await SMS.send('3602248958', 'This is a test message 123x');
+    const res = await SMS.send('3602248958', 'This, right here, right now, is a test message 123x');
     console.log(JSON.stringify(res, null, 2));  
 }
 test();
