df163a3756594b6dfe998977115207c7347ddd5f
Manis Pierre <YvoysePierre@users.noreply.github.com>
2022-12-08 19:27:54 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    accountSid: 'ACe9740307b473cc0c73efb1b50dc69f43',
-    authToken: '9ca4f841cb944503f9c7b4551961ac08',
-    senderNum: '9403505880',
-    adminNums: ['3602248958', '3602969556']
+    accountSid: 'Your Acct SID',
+    authToken: 'authToken from Twilio',
+    senderNum: 'SenderNum from Twilio',
+    adminNums: ['Your or another admin phonenum']
 };
